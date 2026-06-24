139e90b374bb17e045850e4484d386729e8ff2ee
AREF BEYKMOHAMMADI <110643312+arefbeykmohammadi@users.noreply.github.com>
2026-06-24 14:46:39 +0330

    Add files via upload

---
+++ tokenizer.py
@@ -0,0 +1,18 @@
+# TODO: import necessary libraries
+
+class Tokenizer:
+    def __init__(self):
+        pass
+
+# -----------------------
+# Initialize or train tokenizer
+# -----------------------
+def get_tokenizer():
+    """
+    TODO:
+    - create a tokenizer (from pretrained or from scratch)
+    - add special tokens if needed
+    - return the tokenizer object
+    """
+    tokenizer = None
+    return tokenizer
